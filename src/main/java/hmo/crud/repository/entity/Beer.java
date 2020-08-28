05da45311001e99db9d6e3053b9f31499c8c2a24
Helio Marci de Oliveira <heliomolive@hemac.local>
2020-08-27 22:55:20 -0300

    Configure test output on build.gradle

--- src/main/java/hmo/crud/repository/entity/Beer.java
+++ src/main/java/hmo/crud/repository/entity/Beer.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Data
@@ -19,6 +20,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "BEER")
 @DynamicInsert
 @DynamicUpdate
 public class Beer {
